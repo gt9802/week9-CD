74d36b5576b1f30835061f7a4052aacf76a53ea8
gt9802 <thomilangrg@gmail.com>
2023-08-15 10:04:46 +0530

    route

--- server/routes/admin.js
+++ server/routes/admin.js
@@ -74,6 +74,6 @@ router.post('/signup', (req, res) => {
   });
 
   router.get('/hello', authenticateJwt, async(req,res)=>{
-    res.send("hi hello admin")
+    res.send("hi hello admin again")
   })
   module.exports = router
